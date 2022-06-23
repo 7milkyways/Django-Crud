50f0d82890915779fb0c80ed84ed383d02628128
Destiny <omoruyidestiny30@gmail.com>
2022-06-23 06:47:53 +0100

    task complete

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
+from dataclasses import fields
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Post
 # Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(ListView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(ListView):
+    model = Post
+
+class PostUpdateView(ListView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(ListView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
