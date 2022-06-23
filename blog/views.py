d5698b6965b87814e8bc44d43ebada2b83a544e1
Destiny <omoruyidestiny30@gmail.com>
2022-06-23 20:26:28 +0100

    task complete

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from dataclasses import fields
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
+from django.urls import reverse_lazy
 from django.urls import reverse_lazy
 from .models import Post
 # Create your views here.
@@ -9,20 +13,20 @@ from .models import Post
 class PostListView(ListView):
     model = Post
 
-class PostCreateView(ListView):
+class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
 
-class PostDetailView(ListView):
+class PostDetailView(DetailView):
     model = Post
 
-class PostUpdateView(ListView):
+class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
 
-class PostDeleteView(ListView):
+class PostDeleteView(UpdateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
